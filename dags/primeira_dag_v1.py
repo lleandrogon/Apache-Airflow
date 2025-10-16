feba7e5d1f973e225b621eaef0f2a37ac66edeff
root <root@LAPTOP-UPV6P16J.localdomain>
2025-10-16 16:29:33 -0300

    Arquivos padrões para testar DAGs

---
+++ dags/primeira_dag_v1.py
@@ -0,0 +1,17 @@
+from airflow import DAG
+from airflow.providers.standard.operators.empty import EmptyOperator
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime
+
+minha_DAG = DAG(
+    dag_id="a_primeira_DAG_v1",
+    start_date=datetime(2025, 10, 16),
+    schedule="@daily",
+    doc_md=__doc__
+)
+
+start = EmptyOperator(task_id="start", dag=minha_DAG)
+hello = BashOperator(task_id="hello", bash_command="echo hello world", dag=minha_DAG)
+end = EmptyOperator(task_id="end", dag=minha_DAG)
+
+start >> hello >> end
